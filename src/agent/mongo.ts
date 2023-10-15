f943635b6f226793d4e4b50ba62a4a68ad5838d8
mark - desktop <markenkema@gmail.com>
2023-10-15 16:03:02 -0700

    update

--- src/agent/mongo.ts
+++ src/agent/mongo.ts
@@ -2,9 +2,18 @@ import mongoose from "mongoose";
 
 type AgentData = {
   id?: string;
-  listings: string[];
-  name: string;
+  name?: string | null;
   email: string;
+  password?: string;
+  image?: string | null;
+  primaryColor?: string;
+  secondaryColor?: string;
+  logoImage?: string | null;
+  isDeleted?: boolean;
+  socialId?: string;
+  loginType?: string;
+  token?: string;
+  listings: string[];
 };
 
 const AgentSchema = new mongoose.Schema(
