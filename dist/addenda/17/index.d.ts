9e476ed3a61597d7e8f596dfdede7fe8bb70f284
mark - desktop <markenkema@gmail.com>
2023-10-17 22:13:43 -0700

    updated script to overwrite older dist

--- dist/addenda/17/index.d.ts
+++ dist/addenda/17/index.d.ts
@@ -1,3 +1,3 @@
-export { SellerDisclosureDocument, SellerDisclosureDataSchema } from "./mongo";
+export { SellerDisclosureDocument, SellerDisclosureDataSchema, Addenda17Model, } from "./mongo";
 export { Addenda17Data } from "./parsed";
 export { RawSellerDisclosureDocument } from "./raw";
--- dist/addenda/17/index.js
+++ dist/addenda/17/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SellerDisclosureDataSchema = void 0;
+exports.Addenda17Model = exports.SellerDisclosureDataSchema = void 0;
 var mongo_1 = require("./mongo");
 Object.defineProperty(exports, "SellerDisclosureDataSchema", { enumerable: true, get: function () { return mongo_1.SellerDisclosureDataSchema; } });
+Object.defineProperty(exports, "Addenda17Model", { enumerable: true, get: function () { return mongo_1.Addenda17Model; } });
 //# sourceMappingURL=index.js.map
