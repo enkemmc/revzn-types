30ede4ae9de558ec23bbdecc646c72f87b19b8aa
mark - desktop <markenkema@gmail.com>
2023-11-02 11:47:02 -0700

    add timestamp

--- src/offer-bundle/index.ts
+++ src/offer-bundle/index.ts
@@ -49,94 +49,97 @@ const AddendaToDataMap = {
   "22AD": {} as DownpaymentData,
 } as const;
 
-const OfferBundleSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Agent",
-    required: true,
-  },
-  pdfPath: {
-    type: String,
-    required: true,
-  },
-  psa: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "PSA",
-    default: null,
-  },
-  addenda: {
-    "22K": {
+const OfferBundleSchema = new mongoose.Schema(
+  {
+    userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda22K",
-      default: null,
+      ref: "Agent",
+      required: true,
     },
-    "22J": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda22J",
-      default: null,
+    pdfPath: {
+      type: String,
+      required: true,
     },
-    "22E": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda22E",
-      default: null,
-    },
-    "17": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda17",
-      default: null,
-    },
-    "22A": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda22A",
-      default: null,
-    },
-    "21": {
+    psa: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "PSA",
       default: null,
     },
-    "28": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "PSA",
-      default: null,
-    },
-    "22D": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda22D",
-      default: null,
-    },
-    "35": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda35",
-      default: null,
-    },
-    "35E": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda35E",
-      default: null,
-    },
-    "42": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda42",
-      default: null,
-    },
-    "42A": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda42A",
-      default: null,
-    },
-    "22T": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda22T",
-      default: null,
-    },
-    "22AD": {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Addenda22AD",
-      default: null,
+    addenda: {
+      "22K": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda22K",
+        default: null,
+      },
+      "22J": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda22J",
+        default: null,
+      },
+      "22E": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda22E",
+        default: null,
+      },
+      "17": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda17",
+        default: null,
+      },
+      "22A": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda22A",
+        default: null,
+      },
+      "21": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "PSA",
+        default: null,
+      },
+      "28": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "PSA",
+        default: null,
+      },
+      "22D": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda22D",
+        default: null,
+      },
+      "35": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda35",
+        default: null,
+      },
+      "35E": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda35E",
+        default: null,
+      },
+      "42": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda42",
+        default: null,
+      },
+      "42A": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda42A",
+        default: null,
+      },
+      "22T": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda22T",
+        default: null,
+      },
+      "22AD": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Addenda22AD",
+        default: null,
+      },
     },
   },
-});
+  { timestamps: true }
+);
 
 const OfferBundleModel = mongoose.model<OfferBundleData>(
   "OfferBundle",
