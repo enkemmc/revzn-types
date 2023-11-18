05162b057926acdeeb8bc7f4c1dae1d7c4584aff
mark - desktop <markenkema@gmail.com>
2023-11-17 18:52:01 -0800

    removed types

--- dist/socket/index.js
+++ dist/socket/index.js
@@ -3,11 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.SocketChannels = exports.BundleParseEventType = void 0;
 var BundleParseEventType;
 (function (BundleParseEventType) {
+    BundleParseEventType["PSA_FAILED"] = "PSA_FAILED";
+    BundleParseEventType["PSA_COMPLETED"] = "PSA_COMPLETED";
     BundleParseEventType["FETCHING_PDF"] = "FETCHING_PDF";
     BundleParseEventType["PARSING_PSA"] = "PARSING_PSA";
     BundleParseEventType["PARSING_ADDENDA"] = "PARSING_ADDENDA";
-    BundleParseEventType["FAILED"] = "FAILED";
-    BundleParseEventType["COMPLETED"] = "COMPLETED";
+    BundleParseEventType["ADDENDA_FAILED"] = "ADDENDA_FAILED";
+    BundleParseEventType["ADDENDA_COMPLETED"] = "ADDENDA_COMPLETED";
 })(BundleParseEventType || (exports.BundleParseEventType = BundleParseEventType = {}));
 var SocketChannels;
 (function (SocketChannels) {
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -18,49 +18,42 @@ export enum BundleParseEventType {
 }
 
 export interface IPsaParsingFailed {
-  type: SocketChannels;
   status: BundleParseEventType.PSA_FAILED;
   message: string;
   data?: never;
 }
 
 export interface IPsaParsingCompleted {
-  type: SocketChannels;
   status: BundleParseEventType.PSA_COMPLETED;
   message: "PSA parsing completed.";
   data?: never;
 }
 
 export interface IAddendaParsingFailed {
-  type: SocketChannels;
   status: BundleParseEventType.ADDENDA_FAILED;
   message: string;
   data?: never;
 }
 
 export interface IAddendaParsingCompleted {
-  type: SocketChannels;
   status: BundleParseEventType.ADDENDA_COMPLETED;
   message: "Addenda parsing completed.";
   data?: never;
 }
 
 export interface IFetchingPDF {
-  type: SocketChannels.PSA;
   status: BundleParseEventType.FETCHING_PDF;
   message: "Fetching PDF.";
   data?: never;
 }
 
 export interface IParsingPSA {
-  type: SocketChannels.PSA;
   status: BundleParseEventType.PARSING_PSA;
   message: "Parsing PSA.";
   data?: never;
 }
 
 export interface IParsingAddenda {
-  type: SocketChannels.ADDENDA;
   status: BundleParseEventType.PARSING_ADDENDA;
   message: string;
   data?: never;
